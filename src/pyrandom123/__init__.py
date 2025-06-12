0004e8b4965e4cc061b00bb5d4163059901b66fb
Simon Rothman <srothman@wesleyan.edu>
2025-06-12 17:49:12 -0400

    Philox 2x32 generator

--- src/pyrandom123/__init__.py
+++ src/pyrandom123/__init__.py
@@ -7,6 +7,6 @@ pyrandom123: Python bindings for the random123 library of counter-based random n
 from __future__ import annotations
 
 from ._version import version as __version__
-from ._core import add, subtract
+from ._philox2x32 import *
 
-__all__ = ["__version__", "add", "subtract"]
+__all__ = ["__version__"]
